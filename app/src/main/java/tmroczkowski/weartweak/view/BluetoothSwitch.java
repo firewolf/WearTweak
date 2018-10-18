f50fcde813ba8a7f05405ebc50096ff2c5134d04
Tomek Mroczkowski <tmroczkowski@gmail.com>
2018-10-18 23:55:07 +0200

    change switch to imagebuttons, refactorings

--- app/src/main/java/tmroczkowski/weartweak/view/BluetoothSwitch.java
+++ app/src/main/java/tmroczkowski/weartweak/view/BluetoothSwitch.java
@@ -3,6 +3,7 @@ package tmroczkowski.weartweak.view;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.media.Image;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
@@ -24,15 +25,25 @@ public class BluetoothSwitch {
         if (btAdapter != null) {
 
             setState(btAdapter.isEnabled());
-            imageButton.setPressed(getState());
+            changeImage(getState(), imageButton, context);
             imageButton.setOnClickListener(v -> {
                 setState(!getState());
-                v.setPressed(getState());
                 setOnCheckedChangeListener(getState(), btAdapter);
+                changeImage(getState(), imageButton, context);
             });
         } else {
             ((ViewGroup) imageButton.getParent ()).removeView(imageButton);
         }
+    }
+
+    private void changeImage (boolean state, ImageButton imageButton, Context context) {
+
+        int drawable = state ? R.drawable.ic_outline_bluetooth_24px : R.drawable.ic_outline_bluetooth_disabled_24px;
+        int color = state ? R.color.titleButtonCheckedBorder : R.color.semitransparent_grey;
+
+        ((GradientDrawable) imageButton.getBackground()).setStroke(1, context.getColor (color));
+
+        imageButton.setImageDrawable(context.getDrawable(drawable));
     }
 
     private void setState (boolean state) {
--- app/src/main/java/tmroczkowski/weartweak/view/WIFISwitch.java
+++ app/src/main/java/tmroczkowski/weartweak/view/WIFISwitch.java
@@ -2,6 +2,7 @@ package tmroczkowski.weartweak.view;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.net.wifi.WifiManager;
 import android.util.Log;
 import android.widget.CompoundButton;
@@ -12,16 +13,39 @@ import tmroczkowski.weartweak.R;
 
 public class WIFISwitch {
 
+    private boolean state;
+
+    private WifiManager wifiManager;
+
     public WIFISwitch (Context context) {
 
-        final WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 
         ImageButton imageButton = ((Activity) context).findViewById(R.id.buttonWifi);
 
-        imageButton.setPressed(wifiManager.isWifiEnabled());
-        imageButton.setOnClickListener(v -> {
-            v.setPressed (!v.isPressed());
-            wifiManager.setWifiEnabled(v.isPressed());
-        });
+        state = wifiManager.isWifiEnabled();
+
+        changeImage(state, imageButton, context);
+        imageButton.setOnClickListener(v -> setOnClickListener (getState (), imageButton, context));
+    }
+
+    private boolean getState () {
+        return state;
+    }
+
+    private void setOnClickListener (boolean state, ImageButton imageButton, Context context) {
+        this.state = !state;
+        changeImage(this.state, imageButton, context);
+        wifiManager.setWifiEnabled(this.state);
+    }
+
+    private void changeImage (boolean state, ImageButton imageButton, Context context) {
+
+        int drawable = state ? R.drawable.ic_outline_wifi_24px : R.drawable.ic_outline_wifi_off_24px;
+        int color = state ? R.color.titleButtonCheckedBorder : R.color.semitransparent_grey;
+
+        ((GradientDrawable) imageButton.getBackground()).setStroke(1, context.getColor (color));
+
+        imageButton.setImageDrawable(context.getDrawable(drawable));
     }
 }
